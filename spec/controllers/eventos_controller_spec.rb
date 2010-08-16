73dba502d8564af26f350e49002de10d4de62db6
Vinicius Baggio <vinibaggio@gmail.com>
2010-08-16 20:16:04 -0300

    Arrumando erro de sintaxe no eventos controller spec

--- spec/controllers/eventos_controller_spec.rb
+++ spec/controllers/eventos_controller_spec.rb
@@ -8,12 +8,12 @@ describe EventosController do
     @evento.stub!(:aprovado=).and_return(false)
     @evento.stub!(:data=).and_return(Date.today)
     @evento.stub!(:save).and_return(true)
-  end  
-  
+  end
+
   it "deveria cadastrar um evento e deixar com aprovacao pendente e data de inicio = fim caso o fim não tenha sido definido" do
     @evento.stub!(:data).and_return(Date.today)
     @evento.stub!(:data_termino?).and_return(nil)
-    @evento.stub!(:data_termino=).and_return(Date.today)    
+    @evento.stub!(:data_termino=).and_return(Date.today)
     Evento.should_receive(:new).once
     @evento.should_receive(:aprovado=).once
     @evento.should_receive(:data=).once
@@ -27,12 +27,12 @@ describe EventosController do
   it "deveria testar eventos por mes" do
     pending
   end
-  
+
   it "deveria testar eventos por estado" do
     pending
-  end  
-  
-  it "deveria testar a listagem dos twits e imagens dos twitters de determinado evento"
+  end
+
+  it "deveria testar a listagem dos twits e imagens dos twitters de determinado evento" do
     pending
   end
 end
