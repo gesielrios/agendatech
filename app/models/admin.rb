e38c490fa756e6cb8089530a941791d2be1b32fd
Vinicius Baggio <vinibaggio@gmail.com>
2010-08-23 20:59:04 -0300

    Adicionando remember me no attr accessible

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,5 +5,5 @@ class Admin < ActiveRecord::Base
          :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Admin do
-  it { should allow_mass_assignment_of :email, :password, :password_confirmation}
+  it { should allow_mass_assignment_of :email, :password, :password_confirmation, :remember_me }
 end
