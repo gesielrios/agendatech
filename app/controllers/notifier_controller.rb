5ba5a6c85da55bb6455725380e21e3f20e539947
Vinicius Baggio <vinibaggio@gmail.com>
2010-08-23 20:59:34 -0300

    removendo warning de espaco antes dos parenteses

--- app/controllers/notifier_controller.rb
+++ app/controllers/notifier_controller.rb
@@ -2,9 +2,9 @@ class NotifierController < ApplicationController
 
   def notifier
     contato = Contato.new params[:contato]
-    Notifier.deliver_envia_email (contato)
+    Notifier.deliver_envia_email(contato)
     flash[:notice] = "Email enviado com sucesso. Obrigado!"
     redirect_to :controller => 'notifier'
-  end  
+  end
 
 end
