b96b8c98b37f8e194c652cea33e99c1be9901fb9
asouza <alots.ssa@gmail.com>
2010-10-12 00:42:10 -0300

    isolando algumas coisas repetidas

---
+++ lib/plugins.rb
@@ -0,0 +1,18 @@
+class Plugins
+  def self.paper_clip
+    EnvironmentHack.para do |env|
+      env.producao {
+        has_attached_file :logo,           
+          :storage => :s3, 
+          :path => "/:style/:filename",
+          :styles => { :medium => "195x189>", :thumb => "97x97>" }  ,
+          :bucket => ENV['S3_BUCKET'],
+          :s3_credentials => { :access_key_id => ENV['S3_KEY'],                                
+          :secret_access_key => ENV['S3_SECRET'] }          
+      }
+      env.outros {
+        has_attached_file :logo, :styles => { :medium => "195x189>", :thumb => "97x97>" }                
+      }
+    end
+end
+end
