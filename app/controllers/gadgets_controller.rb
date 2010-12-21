39ed224f990f621c43066f4e2f710cef77393652
andersonleite <andersonlfl@gmail.com>
2010-12-21 01:50:40 -0200

    inserindo com ajax ok - next: callback

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -3,6 +3,19 @@ class GadgetsController < ApplicationController
     gadget = Gadget.new :tipo => params[:tipo], :evento_id => params[:evento], :user_id => current_user
     gadget.save
     flash[:gadget] = "OK"
-    redirect_to root_path    
+
+    p "salvou....."
+
+respond_to do |format|
+
+   if request.xhr?
+
+     p "eh xhr,...."
+          format.html { render Evento.where(:id => params[:evento]).first, :layout => false}
+        else
+         format.html { redirect_to(event_url(event), :notice => 'You disliked the proposal.') }
+       end
+end
+    #redirect_to root_path    
   end
 end
