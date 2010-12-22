cc3b4da1af0a169255da73ed307b488757242c8c
Gabriel Oliveira <gabriel.so@gmail.com>
2010-12-22 17:41:37 -0200

    Espero que agora o Gadget Eu Vou funcione...

--- config/application.rb
+++ config/application.rb
@@ -24,23 +24,22 @@ module Agendatech
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = 'pt-BR'
 
     # JavaScript files you want as :defaults (application.js is always included).
-     config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+     config.action_view.javascript_expansions[:defaults] = %w(rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-    config.time_zone = 'UTC'
-    config.i18n.default_locale = :'pt-BR'
-    #para ativar a validacao do formato da data.
+    config.filter_parameters += [:password, :password_confirmation]
+
+    # Para ativar a validacao do formato da data.
     config.use_plugin_parser = true
   end
 end
