dd333352cc11ce6410023145976cd9fee9cef1cb
andersonleite <andersonlfl@gmail.com>
2010-12-23 16:19:56 -0200

    acertando eu vou ajax

--- app/helpers/gadgets_helper.rb
+++ app/helpers/gadgets_helper.rb
@@ -5,7 +5,7 @@ module GadgetsHelper
 
     if user_signed_in?
       if current_user.vai_no? evento
-        content << "Ja vou!"
+        content << link_to('Confirmado!', root_path, :class => 'ajax_vou_confirmado')
       else
         content << link_to( 'Eu vou!', gadgets_path(:evento => evento.id, :tipo => Gadget.tipos[:eu_vou]), :class => 'ajax_vou', :id => "ajax_vou_#{evento.id}", :remote => true     )   
       end  
